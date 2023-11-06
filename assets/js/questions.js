a34e2929db56a2b3306786c83f0cf809f9d9d5d1
SalxIeva <ieva.saldukaite@gmail.com>
2023-11-06 21:54:48 +0000

    starter functions added to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,7 +13,7 @@ var questions = [
         "Hyper Text Markup Language",
         "Hyper Type Markup Language",
         "Hyper Type Micro Language"],
-        correctAnswerIndex: 1
+        correctAnswer: "Hyper Text Markup Language"
     },
     {
         question: "What is CSS(Cascading Style Sheets) used for?",
@@ -21,7 +21,7 @@ var questions = [
         "To interact with a user",
         "To style and add content to web pages",
         "To add images"],
-        correctAnswerIndex: 0
+        correctAnswer: "To style and layout web pages"
     },
     {
         question: "What are the two types of variables in JavaScript?",
@@ -29,7 +29,7 @@ var questions = [
         "Local variables and global variables",
         "Loacl variables and public variables",
         "Dependant variables and independant variables"],
-        correctAnswerIndex: 1
+        correctAnswer: "Local variables and global variables"
     },
     {
         question: "What does API stand for?",
@@ -37,7 +37,7 @@ var questions = [
         "Application Prototype Interface",
         "Application Programming Index",
         "Application Programming Interface"],
-        correctAnswerIndex: 3
+        correctAnswer: "Application Programming Interface"
     },
     {
         question: "Which of the following allows programs and scripts to dynamically access and update the content, structure, and style of a document.",
@@ -45,7 +45,7 @@ var questions = [
         "DOM",
         "API",
         "JSON"],
-        correctAnswerIndex: 1
+        correctAnswer: "DOM"
     },
     {
         question: "What is Git repository?",
@@ -53,6 +53,49 @@ var questions = [
         "It is used to manage multiple versions of source code.",
         "It is a central storage location for managing and tracking changes in files and directories.",
         "It is used to point to an existing code and make a clone or copy of that code in a new directory, at another location. "],
-        correctAnswerIndex: 2
+        correctAnswer: "It is a central storage location for managing and tracking changes in files and directories."
     },
 ]
+// check if questions array works
+console.log(questions);
+
+// check for the right answer
+
+// var questionEl = document.getElementById("questions");
+// var button = document.createElement("button");
+// button.textContent = questions.answers('');
+// document.body.appendChild(button);
+
+// button.addEventListener("click", questions.answers);
+
+// 
+
+var questionEl = document.querySelector("#questions");
+var startBtn = document.querySelector("#start");
+var timeEl = document.querySelector("#time");
+var answerEl = document.querySelector("#choices");
+var currentQuesIndex = 0;
+var timer;
+var timeLeft = 60; // time for every question to answer
+
+
+// Function to display the current question
+function displayQuestion() {
+
+}
+
+// Function to start the quiz
+function startQuiz() {
+
+}
+
+// Function to start the timer
+function startTime() {
+
+}
+// Function to check the selected answer
+function checkAnswer(selectedAnswer, correctAnswer) {
+
+}
+// Add an event listener to the start button to begin the quiz
+startBtn.addEventListener("click", startQuiz);
