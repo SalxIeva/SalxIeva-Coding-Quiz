883be7d4e0e3d24386151d0f89fdf3865654e70e
SalxIeva <ieva.saldukaite@gmail.com>
2023-11-06 17:09:24 +0000

    added questions array to js

---
+++ assets/js/logic.js
@@ -0,0 +1,13 @@
+// check to make sure that js is connected to index.html
+// console.log("hello");
+
+
+// Create a code quiz that contains the following requirements:
+
+// A start button that when clicked a timer starts and the first question appears.
+
+
+// If the answer clicked was incorrect then subtract time from the clock
+// The quiz should end when all questions are answered or the timer reaches 0.
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+// check to make sure that js is connected to index.html
+// console.log("questions");
+
+
+// Questions contain buttons for each answer.
+// When answer is clicked, the next question appears
+
+// questions array
+var questions = [
+    {
+        question: "What is HTML stand for?",
+        answers: ["Hyper Text Module Language",
+        "Hyper Text Markup Language",
+        "Hyper Type Markup Language",
+        "Hyper Type Micro Language"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is CSS(Cascading Style Sheets) used for?",
+        answers: ["To style and layout web pages",
+        "To interact with a user",
+        "To style and add content to web pages",
+        "To add images"],
+        correctAnswerIndex: 0
+    },
+    {
+        question: "What are the two types of variables in JavaScript?",
+        answers: ["Var, For, Const, Let",
+        "Local variables and global variables",
+        "Loacl variables and public variables",
+        "Dependant variables and independant variables"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What does API stand for?",
+        answers: ["Application Programming Information",
+        "Application Prototype Interface",
+        "Application Programming Index",
+        "Application Programming Interface"],
+        correctAnswerIndex: 3
+    },
+    {
+        question: "Which of the following allows programs and scripts to dynamically access and update the content, structure, and style of a document.",
+        answers: ["Javascript",
+        "DOM",
+        "API",
+        "JSON"],
+        correctAnswerIndex: 1
+    },
+    {
+        question: "What is Git repository?",
+        answers: ["It provides an emulation layer for a Git command line experience.",
+        "It is used to manage multiple versions of source code.",
+        "It is a central storage location for managing and tracking changes in files and directories.",
+        "It is used to point to an existing code and make a clone or copy of that code in a new directory, at another location. "],
+        correctAnswerIndex: 2
+    },
+]
