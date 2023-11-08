78ce1408023c72e1e20b052185364c11da1fa935
SalxIeva <ieva.saldukaite@gmail.com>
2023-11-08 09:28:49 +0000

    highScoresLi added to scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,18 @@
 // check to make sure that js is connected to highscores.html
 console.log("scores"); 
 
+// Retrieve the highscores element
+var highScores = document.querySelector("#highscores");
+
+// Function to add a user's score and initials to the highscores list
+function addHighScore(userInitials, totalScore) {
+  // List item is created to store scores
+  var highScoresLi = document.createElement("li");
+  highScoresLi.textContent = userInitials + " - " + totalScore;
+
+  // Append the list item to the highscores list
+  highScores.appendChild(highScoresLi);
+}
 
 // When the game ends, it should display their score and give the user the ability to save their initials and their score
 var totalScore = parseInt(localStorage.getItem(totalScore));
